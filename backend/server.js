4a5d38f01cad2b7dcdada401c465d28d6c641c08
Chintu12a <chintulekurwale@gmail.com>
2023-08-06 23:35:54 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,22 @@ app.use('/api/user', userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+// Deployement------
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname1, "../client/build")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    })
+}
+else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+      });
+}
+
+
 // Deployment
 
 // const __dirname1 = path.resolve();
@@ -34,9 +50,9 @@ app.use("/api/message", messageRoutes);
 //   });
 // }
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running..");
+// });
 
 // Deployment
 
@@ -44,7 +60,7 @@ app.get("/", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5001;
 
 const server = app.listen(PORT,
     console.log(`The server is running ${PORT}`)
